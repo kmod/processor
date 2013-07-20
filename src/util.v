4880b061cf99cab430093de923cba3e2fc0a13b8
Kevin Modzelewski <kevmod@gmail.com>
2013-07-20 13:45:29 -0700

    replace bit-index lookups with a shift register

--- src/util.v
+++ src/util.v
@@ -131,16 +131,16 @@ module uart_tranceiver #(parameter CLK_CYCLES=4167, CTR_WIDTH=16)
 		
 		reg [CTR_WIDTH-1:0] ctr;
 		
-		reg [4:0] bit_idx;
-		reg [9:0] line_data;
-		reg sending;
+		reg [9:0] line_data = 10'b1111111111;
+		reg [4:0] bits_left;
 		
 		initial begin
-			sending = 0;
 			ctr = 0;
+			bits_left = 0;
 		end;
 		
-		assign uart_tx = sending ? line_data[bit_idx] : 1'b1;
+		assign uart_tx = line_data[0];
+		assign ready = (bits_left == 0);
 		
 		always @(posedge clk) begin
 			ctr <= ctr + 1;
@@ -148,18 +148,17 @@ module uart_tranceiver #(parameter CLK_CYCLES=4167, CTR_WIDTH=16)
 			if (ctr == (CLK_CYCLES-1)) begin
 				ctr <= 0;
 				
-				if (bit_idx == 9) begin
-					sending <= 0;
-				end
+				// pop off the just-sent bit (index 0) and shift in a 1:
+				line_data <= {1'b1, line_data[9:1]};
 				
-				bit_idx <= bit_idx + 1;
+				if (bits_left != 0)
+					bits_left <= bits_left - 1'b1;
 			end
 				
-			if (req && !sending) begin
+			if (req) begin
 				line_data <= {1'b1, data, 1'b0};
-				bit_idx <= 0;
-				sending <= 1;
 				ctr <= 0;
+				bits_left <= 4'd10;
 			end
 		end
 endmodule
